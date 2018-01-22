f92d756b58350e44f5afd96355ac95d0ec084726
Shimeng Lin <shimeng_lin@qq.com>
2018-01-22 20:28:16 +0800

    Jan 22

--- point2d.py
+++ point2d.py
@@ -1,12 +1,13 @@
 # coding:UTF-8
 import random
+import math
 
 
 class Point_2D(object):
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __eq__(self, other):
         return (self.x, self.y) == (other.x, other.y)
@@ -24,13 +25,46 @@ class Point_2D(object):
         if self == p:
             return 0.0
         else:
-            return round((((self.x - p.x) ** 2 + (self.y - p.y) ** 2) ** 0.5), 4)
+            return round(math.hypot(self.x - p.x, self.y - p.y), 4)
+
+
+class segment(object):
+
+    def __init__(self, pa, pb):
+        self.pa, self.pb = pa, pb
+        self.len = pa.d(pb)
+        self.k = pb.y - pa.y / pb.x - pa.x
+
+    def __eq__(self, other):
+        return (self.pa, self.pb) == (other.pa, other.pb)
+
+    def __hash__(self):
+        return hash(str(self.pa) + ',' + str(self.pb))
+
+    def __str__(self):
+        return "Segment(%s, %s)" % (self.pa, self.pb)
+
+    def __repr__(self):
+        return "Segment(%s, %s)" % (self.pa, self.pb)
+
+    def minX(self):
+        return min(self.pa.x, self.pb.x)
+
+    def maxX(self):
+        return max(self.pa.x, self.pb.x)
+
+    def minY(self):
+        return min(self.pa.y, self.pb.y)
+
+    def maxY(self):
+        return max(self.pa.y, self.pb.y)
 
 
 # 随机生成点
 def RandomNode(map_size):
     newPoint = Point_2D(random.uniform(0, map_size), random.uniform(0, map_size))
     return newPoint
+
 
 # 批量随机生成点
 def RandomNodeGeneration(map_size, node_amount):
